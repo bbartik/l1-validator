43845ec92d72e177beffe49439fd2a8b12652ebe
Bryan Bartik <bbartik78@gmail.com>
2020-11-24 09:07:23 -0800

    added dedup

--- README.md
+++ README.md
@@ -15,26 +15,15 @@ Enter device username: admin
 Enter device password: 
 
 The following links are missing: 
-PDX-3560-STAGING_GigabitEthernet0/3  >  PDX-3850-03_GigabitEthernet1/0/24    
+leaf5_Ethernet1/1   >  spine1_Ethernet1/5  
+leaf5_Ethernet1/2   >  spine2_Ethernet1/5  
 
 The following links are not in the Master List DB: 
-PDX-3560-STAGING_GigabitEthernet0/3  >  2702GBR_GigabitEthernet0             
-PDX-3850-CORE_GigabitEthernet2/0/6   >  vedge-8004_GigabitEthernet0/0/1      
-PDX-3560-STAGING_GigabitEthernet0/6  >  AP30f7_GigabitEthernet0              
-PDX-3560-STAGING_GigabitEthernet0/9  >  PDX-3850-02_GigabitEthernet0/0       
-PDX-3850-02_GigabitEthernet0/0       >  PDX-3650-STAGING_GigabitEthernet0/9  
-PDX-3850-02_GigabitEthernet1/0/13    >  fg-edge1_GigabitEthernet0/0          
-PDX-3850-02_GigabitEthernet1/0/14    >  fg-edge2_GigabitEthernet0/0   
+leaf2_Ethernet1/7  >  leaf1_Ethernet1/7    
 ```
 
 ## Notes:
-- Uses custom CDP TextFSM template to grab necessary data
+- Uses custom CDP TextFSM template to grab necessary data, you may need to modify.
 
-## To-do:
-- Need to remove duplicates so you don't need A's and B's view of the links
 
-For example, you need both these entries in master.yaml. How to dedup this?
-
- - [ PDX-3850-02_GigabitEthernet1/0/24, PDX-3650-STAGING_GigabitEthernet0/2 ]
- - [ PDX-3560-STAGING_GigabitEthernet0/2, PDX-3850-02_GigabitEthernet1/0/24 ]
 
--- l1_validator.py
+++ l1_validator.py
@@ -7,21 +7,25 @@ import textfsm
 from ntc_templates.parse import parse_output
 import pprint
 from ruamel.yaml import YAML
+import numpy as np
 import pdb
 import getpass
+import sys
 
 yaml = YAML()
 nr = InitNornir(config_file="config.yaml")
-nbr_list = []
 pp = pprint.PrettyPrinter(indent=4)
 
 # you can also set these in the inventory/defaults.yaml file
-username = input("\nEnter device username: ")
-password = getpass.getpass("Enter device password: ")
-nr.inventory.defaults.username = username
-nr.inventory.defaults.password = password
+#username = input("\nEnter device username: ")
+#password = getpass.getpass("Enter device password: ")
+nr.inventory.defaults.username = "admin"
+nr.inventory.defaults.password = "cisco123"
 
-def l1_validate(task, nbr_list):
+def duplicate(connections):
+    return None
+
+def l1_validate(task):
 
     cmd = "show cdp neighbors detail"
     output = task.run(task=netmiko_send_command, command_string=cmd)
@@ -29,52 +33,93 @@ def l1_validate(task, nbr_list):
 
     with open("cisco_ios_show_cdp_detail.textfsm") as f:
         template = textfsm.TextFSM(f)
-    cdp_results = template.ParseText(cdp_results)
-
+    parsed_results = template.ParseText(cdp_results)
+    
+    # initialize neighbor list
+    nbr_list = []
     local_host = str(task.host.name)
 
-    for nbr in cdp_results:
+    for nbr in parsed_results:
         # strip of domain
         neighbor_hostname = nbr[0].split(".")[0]
 
-        # create tuple
+        # create link set
         local_intf = f"{local_host}_{nbr[2]}"
         remote_intf = f"{neighbor_hostname}_{nbr[3]}"
-        nbr_tuple = (local_intf, remote_intf)
+        if "mgmt" not in local_intf:
+            nbr_set = {local_intf, remote_intf}
 
-        # append tuple to list of actual links
-        nbr_list.append(nbr_tuple)
+            # append tuple to list of actual links
+            nbr_list.append(nbr_set)
 
-# filter only campus switches
-campus_switches = nr.filter(F(groups__contains="campus"))
-r = campus_switches.run(task=l1_validate, nbr_list=nbr_list)
+    return nbr_list
+
+
+# initialize live complete neighbor list
+live_nbr_list = []
+# filter if needed
+#campus_switches = nr.filter(F(groups__contains="datacenter"))
+result = nr.run(task=l1_validate)
+
+if len(result.failed_hosts) > 0:
+    print("One or more hosts failed to connect. Please run again.")
+    print(result.failed_hosts)
+    sys.exit()
+
+# populate new list from all nodes
+for x in result.keys():
+    x_links = result[x].result
+    for link in x_links:
+        live_nbr_list.append(link)
+
+# remove duplicates, this is the set that will be used for comparison
+live_nbr_set = set()
+for nbr in live_nbr_list:
+    live_nbr_set.add(tuple(nbr))
+temp_live_nbr_list = list(live_nbr_set)
+live_nbr_list = [set(x) for x in temp_live_nbr_list ]
 
 # load master list of links
-with open("master_list.yaml", "r") as f:
-    master_list = f.read()
+with open("l1_spine_leaf.yaml", "r") as f:
+    master_file = f.read()
 
 # initialize empty set of lists, create set of tuples for set thoery operations
-master_set = []
-connections = yaml.load(master_list)
-for c in connections:
-    master_set.append(tuple(c))
-master_set = set(master_set)
-nbr_set = set(nbr_list)
+master_list = []
+connections = yaml.load(master_file)
+for conn in connections:
+    #pdb.set_trace()
+    # convert to set to normalize the order
+    conn = set(conn)
+    # convert to tuple so you can add it to the list and convert to set
+    master_list.append(conn)
 
-# subtract existing set from master set to find missing links
-missing = master_set - nbr_set
-not_in_db = nbr_set - master_set
+'''
+deugging the set order issue
+print(len(master_set))
+pprint.pprint(master_set)
+sys.exit()
+'''
 
-print("\nThe following links are missing: ")
+# subtract existing list from master list to find missing links
+missing = [x for x in master_list if x not in live_nbr_list]
+not_in_db = [x for x in live_nbr_list if x not in master_list]
+
 # pretty printing the columns
-col_width = max(len(word) for link in missing for word in link) + 2  # padding
-for link in missing:
-    #link = list(link)
-    print(">  ".join(word.ljust(col_width) for word in link))
+try:
+    print("\nThe following links are missing: ")
+    col_width = max(len(word) for link in missing for word in link) + 2  # padding
+    for link in missing:
+        #link = list(link)
+        print(">  ".join(word.ljust(col_width) for word in link))
+except:
+    print("Looks like there are no missing links!")
 
 print("\nThe following links are not in the Master List DB: ")
 # pretty printing the columns
-col_width = max(len(word) for link in not_in_db for word in link) + 2  # padding
-for link in not_in_db:
-    #link = list(link)
-    print(">  ".join(word.ljust(col_width) for word in link))
+try:
+    col_width = max(len(word) for link in not_in_db for word in link) + 2  # padding
+    for link in not_in_db:
+        #link = list(link)
+        print(">  ".join(word.ljust(col_width) for word in link))
+except:
+    print("Looks like all links are in the Master DB")
